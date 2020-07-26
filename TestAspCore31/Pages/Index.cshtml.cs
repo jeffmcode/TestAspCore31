949b15978af2cadc8e143fc63a775af5d59610be
Jeff M <jmmisc@hotmail.com>
2020-07-26 14:34:15 -0500

    First commit

---
+++ TestAspCore31/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace TestAspCore31.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public DateTime CurrentTime { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            CurrentTime = DateTime.Now;
+        }
+    }
+}
